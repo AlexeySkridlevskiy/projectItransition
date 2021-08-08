1983fb26e571bafd2071cd201a05a95d773ea8f3
AlexeySkridlevskiy <skrid71@gmail.com>
2021-08-08 16:54:51 +0300

    start files

---
+++ src/main/java/com/example/projectitransition/ProjectItransitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectitransition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectItransitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectItransitionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectitransition/ProjectItransitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectitransition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectItransitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
